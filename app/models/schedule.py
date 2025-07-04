053ebb2f2f98aa4cffa5c95edd7c163bc5fa86a6
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 11:06:17 -0300

    fix: schedule

--- app/models/schedule.py
+++ app/models/schedule.py
@@ -2,29 +2,28 @@
 
 from datetime import datetime
 
+from sqlalchemy import Boolean, DateTime, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.db.db import Base as db
+from app.db.db import Base
 
 
-class ScheduleService(db.Model):
+class ScheduleService(Base):
     __tablename__ = "schedule_service"
     __table_args__ = {"schema": "public"}
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    time_register: Mapped[datetime] = mapped_column(
-        db.DateTime, nullable=False
-    )
-    updated_at: Mapped[datetime] = mapped_column(db.DateTime, nullable=True)
-    updated_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
-    deleted_at: Mapped[datetime] = mapped_column(db.DateTime, nullable=True)
-    deleted_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
-    product_id: Mapped[int] = mapped_column(db.Integer, nullable=False)
-    employee_id: Mapped[int] = mapped_column(db.Integer, nullable=False)
-    user_id: Mapped[int] = mapped_column(db.Integer, nullable=False)
-    is_check: Mapped[int] = mapped_column(db.Boolean, nullable=False)
-    is_awayalone: Mapped[int] = mapped_column(db.Boolean, nullable=False)
-    is_deleted: Mapped[bool] = mapped_column(db.Boolean, default=False)
+    time_register: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    updated_by: Mapped[int] = mapped_column(Integer, nullable=True)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    deleted_by: Mapped[int] = mapped_column(Integer, nullable=True)
+    product_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    employee_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    user_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    is_check: Mapped[int] = mapped_column(Boolean, nullable=False)
+    is_awayalone: Mapped[int] = mapped_column(Boolean, nullable=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
     def __repr__(self):
         return f"""{self.id} created successfully"""
