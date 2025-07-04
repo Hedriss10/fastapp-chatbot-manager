e63bb56d6284944eed8ca8f392a2ded21b973aa9
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 17:40:29 -0300

    add: folder messages in project

---
+++ app/messages/welcome.py
@@ -0,0 +1,10 @@
+# app/core/welcome.py
+
+from app.db.db import Session
+
+class WelcomeCore:
+    def __init__(self, db: Session):
+        self.db = db
+        
+        
+    def send_message(self): ...
