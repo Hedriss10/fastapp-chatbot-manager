f2e625f0225ea778bda5ab31aecf47844fd06ff9
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-03 23:04:41 -0300

    fix: adjust depency database manager

---
+++ app/db/depency.py
@@ -0,0 +1,11 @@
+# app/db/depency.py
+
+from sqlalchemy.orm import Session
+from app.db.db import engine, SessionLocal
+
+def get_db():
+    db: Session = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
