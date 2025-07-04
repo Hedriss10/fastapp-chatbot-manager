8d4712c7e339c008b01afd791a72c0a4661f49cf
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 17:37:02 -0300

    add: messages and messages flow

--- app/models/messages.py
+++ app/models/messages.py
@@ -7,10 +7,12 @@ from sqlalchemy import (
     JSON,
     Boolean,
     DateTime,
+    ForeignKey,
     Integer,
+    String,
     func,
 )
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 class SummaryMessage(Base):
@@ -18,6 +20,7 @@ class SummaryMessage(Base):
     __table_args__ = {"schema": "campaign"}
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    ticket: Mapped[str] = mapped_column(String(40), nullable=False)
     message: Mapped[str] = mapped_column(JSON, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, nullable=False, server_default=func.now()
@@ -28,5 +31,50 @@ class SummaryMessage(Base):
     deleted_by: Mapped[int] = mapped_column(Integer, nullable=True)
     is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
+    message_flows: Mapped[list["MessageFlow"]] = relationship(
+        back_populates="summary",
+        foreign_keys="MessageFlow.summary_id",
+        cascade="all, delete-orphan",
+    )
+
+    next_flows: Mapped[list["MessageFlow"]] = relationship(
+        back_populates="next_message",
+        foreign_keys="MessageFlow.next_message_id",
+    )
+
     def __repr__(self):
         return f"SummaryMessage(id={self.id}, message={self.message})"
+
+
+class MessageFlow(Base):
+    __tablename__ = "message_flow"
+    __table_args__ = {"schema": "campaign"}
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    summary_id: Mapped[int] = mapped_column(
+        Integer,
+        ForeignKey("campaign.summary_message.id", ondelete="CASCADE"),
+        nullable=False,
+    )
+
+    next_message_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("campaign.summary_message.id"), nullable=False
+    )
+
+    option_number: Mapped[int] = mapped_column(Integer, nullable=False)
+    option_label: Mapped[str] = mapped_column(String(30), nullable=False)
+    action_type: Mapped[str] = mapped_column(String(30), nullable=False)
+    action_payload: Mapped[str] = mapped_column(JSON, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime, nullable=False, server_default=func.now()
+    )
+
+    # Relacionamentos
+    summary: Mapped["SummaryMessage"] = relationship(
+        back_populates="message_flows", foreign_keys=[summary_id]
+    )
+
+    next_message: Mapped["SummaryMessage"] = relationship(
+        back_populates="next_flows", foreign_keys=[next_message_id]
+    )
