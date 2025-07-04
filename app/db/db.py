7063ece75172c2c035766ddba84d8dbb8493cc93
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-03 23:01:12 -0300

    fix: add mdoels user and ruff in requirements

---
+++ app/db/db.py
@@ -0,0 +1,13 @@
+# app/db/db.py
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+load_dotenv()
+SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URI)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
