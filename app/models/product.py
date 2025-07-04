6550ee52c297e670c78b126c530ebcc0c96c841a
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 11:04:30 -0300

    fix: adjust product

--- app/models/product.py
+++ app/models/product.py
@@ -2,29 +2,37 @@
 
 from datetime import datetime
 
-from sqlalchemy import Interval, Numeric
+from sqlalchemy import (
+    Boolean,
+    DateTime,
+    Float,
+    Integer,
+    Interval,
+    Numeric,
+    String,
+)
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.db.db import Base as db
+from app.db.db import Base
 
 
-class Products(db.Model):
+class Products(Base):
     __tablename__ = "products"
     __table_args__ = {"schema": "finance"}
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    description: Mapped[str] = mapped_column(db.String(30), nullable=False)
+    description: Mapped[str] = mapped_column(String(30), nullable=False)
     value_operation: Mapped[Numeric] = mapped_column(
-        db.Numeric(2, 10), default=0.00
+        Numeric(2, 10), default=0.00
     )
     time_to_spend: Mapped[Interval] = mapped_column(Interval, nullable=False)
-    commission: Mapped[float] = mapped_column(db.Float, nullable=False)
-    category: Mapped[str] = mapped_column(db.String(20), nullable=False)
-    updated_at: Mapped[datetime] = mapped_column(db.DateTime, nullable=True)
-    updated_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
-    deleted_at: Mapped[datetime] = mapped_column(db.DateTime, nullable=True)
-    deleted_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
-    is_deleted: Mapped[bool] = mapped_column(db.Boolean, default=False)
+    commission: Mapped[float] = mapped_column(Float, nullable=False)
+    category: Mapped[str] = mapped_column(String(20), nullable=False)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    updated_by: Mapped[int] = mapped_column(Integer, nullable=True)
+    deleted_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+    deleted_by: Mapped[int] = mapped_column(Integer, nullable=True)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
     def __repr__(self):
         return f"""{self.description} created successfully"""
