e1c5b106bd17489660a6bb20f3578106afa29787
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 11:02:26 -0300

    fix: adjust employee

--- app/models/employee.py
+++ app/models/employee.py
@@ -2,35 +2,33 @@
 
 from datetime import datetime
 
-from sqlalchemy import func
+from sqlalchemy import Boolean, DateTime, Integer, String, Text, func
 from sqlalchemy.orm import Mapped, mapped_column
 
-from app.db.db import Base as db
+from app.db.db import Base
 
 
-class Employee(db.Model):
+class Employee(Base):
     __tablename__ = "employee"
     __table_args__ = {"schema": "public"}
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str] = mapped_column(db.String(120), nullable=False)
-    date_of_birth: Mapped[datetime] = mapped_column(
-        db.DateTime, nullable=False
-    )
-    phone: Mapped[str] = mapped_column(db.String(40), nullable=False)
-    role: Mapped[str] = mapped_column(db.String(40), nullable=False)
-    session_token: Mapped[str] = mapped_column(db.Text, nullable=True)
-    password: Mapped[str] = mapped_column(db.String(300), nullable=False)
+    username: Mapped[str] = mapped_column(String(120), nullable=False)
+    date_of_birth: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    phone: Mapped[str] = mapped_column(String(40), nullable=False)
+    role: Mapped[str] = mapped_column(String(40), nullable=False)
+    session_token: Mapped[str] = mapped_column(Text, nullable=True)
+    password: Mapped[str] = mapped_column(String(300), nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        db.DateTime, nullable=False, server_default=func.now()
+        DateTime, nullable=False, server_default=func.now()
     )
-    updated_at: Mapped[str] = mapped_column(db.DateTime, nullable=True)
-    updated_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
-    deleted_by: Mapped[int] = mapped_column(db.Integer, nullable=True)
+    updated_at: Mapped[str] = mapped_column(DateTime, nullable=True)
+    updated_by: Mapped[int] = mapped_column(Integer, nullable=True)
+    deleted_by: Mapped[int] = mapped_column(Integer, nullable=True)
     deleted_at: Mapped[datetime] = mapped_column(
-        db.DateTime, nullable=True, server_default=func.now()
+        DateTime, nullable=True, server_default=func.now()
     )
-    is_deleted: Mapped[bool] = mapped_column(db.Boolean, default=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean, default=False)
 
     def __repr__(self):
         return f"""{self.username} created employeesuccessfully"""
