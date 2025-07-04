801bbb2b1a210478fa01287bf4c4c549da402b6b
Hedris Pereira <hedris.pereira@eemovel.com>
2025-07-04 08:37:36 -0300

    :broom: cleanup with ruff

--- app/db/depency.py
+++ app/db/depency.py
@@ -1,7 +1,7 @@
 # app/db/depency.py
 
 from sqlalchemy.orm import Session
-from app.db.db import engine, SessionLocal
+from app.db.db import SessionLocal
 
 def get_db():
     db: Session = SessionLocal()
