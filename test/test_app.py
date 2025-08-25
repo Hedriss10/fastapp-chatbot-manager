a97fc02623f39e4c3c4d67e2b03c94631a1c4362
Hedris Pereira <hedris.pereira@eemovel.com>
2025-08-25 19:31:29 -0300

    hotfix: adjust fix

--- test/test_app.py
+++ test/test_app.py
@@ -3,8 +3,3 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
-
-
-def test_read_main():
-    response = client.get('/heartcheck')
-    assert response.status_code == 200
